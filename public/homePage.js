f6b6599a4155c33f5ce8bcd3c43a898e74f90f78
Kiyas <kiiyas@yandex.ru>
2021-04-04 01:53:39 +0500

    some duplicate code fixed

--- public/homePage.js
+++ public/homePage.js
@@ -4,26 +4,23 @@
 const logoutBtn = new LogoutButton;
 logoutBtn.action = function () {
     ApiConnector.logout( callback => {        
-        if (callback.success) {
-            location.reload();
-        } 
+        if (callback.success) { location.reload() } 
     })
 }
 
-//Получение информации о пользователе
-ApiConnector.current(callback => {
-    //console.log(callback);
-    if (callback.success) {
-        ProfileWidget.showProfile(callback.data);
-    }
-});
+function showProfileData (data) {
+  ProfileWidget.showProfile(data);
+}
 
+//Получение информации о пользователе
+ApiConnector.current(callback => {    
+    if (callback.success) { showProfileData(callback.data) }
+});
 
 //Получение текущих курсов валюты
 const ratesBoard = new RatesBoard();
 function getValuteCurses() {
-  ApiConnector.getStocks(callback => {
-    //console.log(callback);
+  ApiConnector.getStocks(callback => {    
     if (callback.success) {      
       ratesBoard.clearTable();
       ratesBoard.fillTable(callback.data);
@@ -41,10 +38,9 @@ const moneyManager = new MoneyManager();
 
 //Пополнение баланса
 moneyManager.addMoneyCallback = function (data) {
-    ApiConnector.addMoney(data, callback => {
-        //console.log(callback);
+    ApiConnector.addMoney(data, callback => {        
         if (callback.success) {
-            ProfileWidget.showProfile(callback.data);
+            showProfileData(callback.data);
             moneyManager.setMessage(callback.success, 'Баланс пополнен!');
         } else {
             moneyManager.setMessage(callback.error, 'Введите сумму!');
@@ -54,10 +50,9 @@ moneyManager.addMoneyCallback = function (data) {
 
 //Конвертирование валюты
 moneyManager.conversionMoneyCallback = function (data) {
-    ApiConnector.convertMoney(data, callback => {
-        //console.log(callback);
+    ApiConnector.convertMoney(data, callback => {        
         if (callback.success) {
-            ProfileWidget.showProfile(callback.data);
+            showProfileData(callback.data);
             moneyManager.setMessage(callback.success, 'Конвертация выполнена!')
         } else {
             moneyManager.setMessage(callback.error, 'Введите суммы!');
@@ -67,10 +62,9 @@ moneyManager.conversionMoneyCallback = function (data) {
 
 //Перевод валюты
 moneyManager.sendMoneyCallback = function (data) {
-    ApiConnector.transferMoney(data, callback => {  
-        //console.log(callback);      
+    ApiConnector.transferMoney(data, callback => {               
         if (callback.success) {
-            ProfileWidget.showProfile(callback.data);
+            showProfileData(callback.data);
             moneyManager.setMessage(callback.success, 'Перевод выполнен!')
         } else {
             moneyManager.setMessage(callback.error, 'Не введена сумма или получатель не найден!');
@@ -82,25 +76,25 @@ moneyManager.sendMoneyCallback = function (data) {
 //            РАБОТА С ИЗБРАННЫМ
 
 const favoritesWidget = new FavoritesWidget();
+function getUpdate(data) {
+  favoritesWidget.fillTable(data);
+  moneyManager.updateUsersList(data);
+}
 
-ApiConnector.getFavorites(callback => { //начальный список избранного
-    //console.log(callback);
+ApiConnector.getFavorites(callback => { //начальный список избранного    
     if (callback.success) {
       favoritesWidget.clearTable();
-      favoritesWidget.fillTable(callback.data);
-      moneyManager.updateUsersList(callback.data); //выпадающий список для перевода денег
+      getUpdate(callback.data);
     }   
   });
   
   //добавления пользователя в список избранных
   favoritesWidget.addUserCallback = function (data) {
-      ApiConnector.addUserToFavorites(data, callback => {
-        //console.log(callback);
+      ApiConnector.addUserToFavorites(data, callback => {        
         if (callback.success) {
           favoritesWidget.setMessage(callback.success, 'Пользователь добавлен в список избраннных!');
           favoritesWidget.clearTable();
-          favoritesWidget.fillTable(callback.data);
-          moneyManager.updateUsersList(callback.data);
+          getUpdate(callback.data);
         } else {
           favoritesWidget.setMessage(callback.error, 'Заполните все поля информации!');
         }
@@ -110,14 +104,12 @@ ApiConnector.getFavorites(callback => { //начальный список изб
   //удаление пользователя из избранного
   favoritesWidget.removeUserCallback = function (data) {
       ApiConnector.removeUserFromFavorites(data, callback => {
-        //console.log(callback);
         if (callback.success) {
           favoritesWidget.setMessage(callback.success, 'Пользователь удален из списка избранного!');
           favoritesWidget.clearTable();
-          favoritesWidget.fillTable(callback.data);
-          moneyManager.updateUsersList(callback.data);
+          getUpdate(callback.data);
         } else {
-          favoritesWidget.setMessage(callback.error, 'эээ!');
+          favoritesWidget.setMessage(callback.error);
         }
       });
     }
